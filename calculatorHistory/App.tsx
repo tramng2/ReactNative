05cb20bc3f35d8495d623ebfdb8b3f88f40c0e74
TramNguyen <tramtram2205@gmail.com>
2021-09-07 18:42:28 +0300

    Refactor code, replace some duplicate code.

--- calculatorHistory/App.tsx
+++ calculatorHistory/App.tsx
@@ -1,67 +1,69 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
+import { StatusBar } from 'expo-status-bar'
+import React, { useState, useEffect } from 'react'
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   Button,
-  FlatList,
-} from "react-native";
+  FlatList
+} from 'react-native'
 interface Data {
-  content: string;
+  content: string
 }
-export default function App() {
-  const [num1, setNum1] = useState<string>();
-  const [num2, setNum2] = useState<string>();
-  const [sign, setSign] = useState<string>();
-  const [result, setResult] = useState<number>();
-  const [data, setData] = useState<Data[]>([]);
+export default function App () {
+  const [num1, setNum1] = useState<string>()
+  const [num2, setNum2] = useState<string>()
+  const [sign, setSign] = useState<string>()
+  const [result, setResult] = useState<number>()
+  const [data, setData] = useState<Data[]>([])
 
   useEffect(() => {
     if (result && sign) {
-      setData([...data, { content: `${num1} ${sign} ${num2} = ${result}` }]);
-      setNum1("");
-      setNum2("");
-      setSign("");
+      setData([...data, { content: `${num1} ${sign} ${num2} = ${result}` }])
+      setNum1('')
+      setNum2('')
+      setSign('')
     }
-  }, [result]);
+  }, [result])
 
   const covertDecimal = (numberStr: any) => {
-    const decimal: any = numberStr;
-    return parseFloat(decimal.replace(",", "."));
-  };
-  const handlePlus = () => {
-    setResult(covertDecimal(num1) + covertDecimal(num2));
-    setSign("+");
-  };
-  const handleMinus = () => {
-    setResult(covertDecimal(num1) + covertDecimal(num2));
-    setSign("-");
-  };
+    const decimal: any = numberStr
+    return parseFloat(decimal.replace(',', '.'))
+  }
+  const handleOperation = (sign: string) => {
+    if (sign === '+') {
+      setResult(covertDecimal(num1) + covertDecimal(num2))
+      setSign('+')
+    } else if (sign === '-') {
+      setResult(covertDecimal(num1) + covertDecimal(num2))
+      setSign('-')
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.container}>
         <Text>{result}</Text>
         <TextInput
           style={styles.textInput}
-          onChangeText={(input) => setNum1(input)}
+          onChangeText={input => setNum1(input)}
           value={num1}
         />
         <TextInput
           style={styles.textInput}
-          onChangeText={(input) => setNum2(input)}
+          onChangeText={input => setNum2(input)}
           value={num2}
         />
         <View style={styles.btn}>
           <Button
-            title="+"
-            onPress={handlePlus}
+            title='+'
+            onPress={() => handleOperation('+')}
             disabled={num1 && num2 ? false : true}
           />
           <Button
-            title="-"
-            onPress={handleMinus}
+            title='-'
+            onPress={() => handleOperation('-')}
             disabled={num1 && num2 ? false : true}
           />
         </View>
@@ -74,30 +76,30 @@ export default function App() {
           keyExtractor={(item, index) => index.toString()}
         />
       </View>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-end",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
   },
   listcontainer: {
     flex: 2,
     padding: 10,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   textInput: {
     width: 200,
-    borderColor: "gray",
-    borderWidth: 1,
+    borderColor: 'gray',
+    borderWidth: 1
   },
   btn: {
-    flexDirection: "row",
-  },
-});
+    flexDirection: 'row'
+  }
+})
