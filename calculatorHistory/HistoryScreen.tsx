2776d20d3f6cdbb800c7e053c7b49757b18f215f
TramNguyen <tramtram2205@gmail.com>
2021-09-15 11:00:17 +0300

    Fix bug typing.

--- calculatorHistory/HistoryScreen.tsx
+++ calculatorHistory/HistoryScreen.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
-import { RootStackParamList } from './App'
-import { StackNavigationProp } from '@react-navigation/stack'
+import React from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { RootStackParamList } from "./App";
+import { StackNavigationProp } from "@react-navigation/stack";
 
 type Props = {
-  navigation: StackNavigationProp<RootStackParamList, 'Home'>
-  route: StackNavigationProp<RootStackParamList, 'Home'>
-}
-export default function HistoryScreen ({ route, navigation }: Props) {
-  const { operation } = route.params
+  route: StackNavigationProp<RootStackParamList, "History">;
+};
+export default function HistoryScreen(route: any) {
+  const { operation } = route.params;
   return (
     <View style={styles.listContainer}>
       <Text>History</Text>
@@ -18,14 +17,14 @@ export default function HistoryScreen ({ route, navigation }: Props) {
         keyExtractor={(item, index) => index.toString()}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   listContainer: {
     flex: 2,
     padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- calculatorHistory/HomeScreen.tsx
+++ calculatorHistory/HomeScreen.tsx
@@ -1,91 +1,90 @@
-import React, { useState, useEffect } from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
-import { StackNavigationProp } from '@react-navigation/stack'
-import { RootStackParamList, Data } from './App'
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList, Data } from "./App";
 
 type Props = {
-  navigation: StackNavigationProp<RootStackParamList, 'Home'>
-}
+  navigation: StackNavigationProp<RootStackParamList, "Home">;
+};
 
-export default function HomeScreen ({ navigation }: Props) {
-  const [num1, setNum1] = useState<string>()
-  const [num2, setNum2] = useState<string>()
-  const [sign, setSign] = useState<string>()
-  const [result, setResult] = useState<number>()
-  const [data, setData] = useState<Data[]>([])
+export default function HomeScreen({ navigation }: Props) {
+  const [num1, setNum1] = useState<string>();
+  const [num2, setNum2] = useState<string>();
+  const [sign, setSign] = useState<string>();
+  const [result, setResult] = useState<number>();
+  const [data, setData] = useState<Data[]>([]);
 
   useEffect(() => {
-    console.log(sign)
     if (sign) {
-      setData([...data, { content: `${num1} ${sign} ${num2} = ${result}` }])
-      setNum1('')
-      setNum2('')
-      setSign('')
+      setData([...data, { content: `${num1} ${sign} ${num2} = ${result}` }]);
+      setNum1("");
+      setNum2("");
+      setSign("");
     }
-  }, [result, sign])
+  }, [result, sign]);
   const covertDecimal = (numberStr: any) => {
-    const decimal: any = numberStr
-    return parseFloat(decimal.replace(',', '.'))
-  }
+    const decimal: any = numberStr;
+    return parseFloat(decimal.replace(",", "."));
+  };
   const handleOperation = (sign: string) => {
-    if (sign === '+') {
-      setResult(covertDecimal(num1) + covertDecimal(num2))
-      setSign('+')
-    } else if (sign === '-') {
-      setResult(covertDecimal(num1) - covertDecimal(num2))
-      setSign('-')
+    if (sign === "+") {
+      setResult(covertDecimal(num1) + covertDecimal(num2));
+      setSign("+");
+    } else if (sign === "-") {
+      setResult(covertDecimal(num1) - covertDecimal(num2));
+      setSign("-");
     }
-  }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.container}>
         <Text>Result: {result}</Text>
         <TextInput
           style={styles.textInput}
-          onChangeText={input => setNum1(input)}
+          onChangeText={(input) => setNum1(input)}
           value={num1}
         />
         <TextInput
           style={styles.textInput}
-          onChangeText={input => setNum2(input)}
+          onChangeText={(input) => setNum2(input)}
           value={num2}
         />
         <View style={styles.btn}>
           <Button
-            title='+'
-            onPress={() => handleOperation('+')}
+            title="+"
+            onPress={() => handleOperation("+")}
             disabled={num1 && num2 ? false : true}
           />
           <Button
-            title='-'
-            onPress={() => handleOperation('-')}
+            title="-"
+            onPress={() => handleOperation("-")}
             disabled={num1 && num2 ? false : true}
           />
           <Button
-            title='History'
-            onPress={() => navigation.navigate('History', { operation: data })}
+            title="History"
+            onPress={() => navigation.navigate("History", { operation: data })}
           />
         </View>
       </View>
-      <StatusBar style='auto' />
+      <StatusBar style="auto" />
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   textInput: {
     width: 200,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
-    padding: 5
+    padding: 5,
   },
   btn: {
-    flexDirection: 'row'
-  }
-})
+    flexDirection: "row",
+  },
+});
