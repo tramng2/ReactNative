2002d2c7d35cd4a46daeb4bdbd8fe82826e181e2
TramNguyen <tramtram2205@gmail.com>
2021-09-19 19:15:35 +0300

    Style and add logic to convert button.

--- euro-convert/screens/HomeScreen.tsx
+++ euro-convert/screens/HomeScreen.tsx
@@ -5,11 +5,11 @@ import { StyleSheet, Text, View, Image, Button, TextInput } from "react-native";
 import { CurrencyData } from "../types";
 
 function HomeScreen() {
-  //   const [currency, setCurrency] = useState<CurrencyData>();
   const [currencyList, setCurrencyList] = useState<CurrencyData>({});
   const [selectedCurrency, setSelectedCurrency] = useState<ItemValue>("");
   const [input, setInput] = useState<string>("");
-  const [euroConverted, setEuroCoverted] = useState<Number>(0);
+  const [euroConverted, setEuroCoverted] = useState<number>(0);
+  const [indexCurrency, setIndexCurrency] = useState<number>(0);
 
   useEffect(() => {
     async function fetchData() {
@@ -24,61 +24,83 @@ function HomeScreen() {
 
   const handleConvert = async () => {
     const euroAmount = Number(input) * Number(selectedCurrency);
-    setEuroCoverted(euroAmount);
+    setEuroCoverted(Math.round(euroAmount * 100) / 100);
+    setInput("");
   };
+
   return (
-    <View style={styles.homeScreen}>
-      <View style={styles.homeScreen_result}>
+    <View>
+      <View style={styles.header}>
         <Image
           source={{
             uri: "https://images.unsplash.com/photo-1604594849809-dfedbc827105?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
             width: 200,
-            height: 200,
+            height: 100,
           }}
         />
-        <Text>{euroConverted}€</Text>
+        <Text style={styles.text}>{euroConverted}€</Text>
+      </View>
+      <View style={styles.input}>
         <TextInput
+          style={styles.textInput}
           value={input}
           onChangeText={(input) => setInput(input)}
           placeholder="Type"
+          keyboardType="numeric"
         />
+        <Text style={styles.textCurrencyUnit}>
+          {Object.keys(currencyList)[indexCurrency]}
+        </Text>
       </View>
-      <View style={styles.button}>
+      <View style={styles.currencyPicker}>
+        <Button
+          title="CONVERT"
+          onPress={() => handleConvert()}
+          disabled={input ? false : true}
+        />
+
         <Picker
           selectedValue={selectedCurrency}
-          onValueChange={(itemValue: any, itemIndex) =>
-            setSelectedCurrency(itemValue)
-          }
+          onValueChange={(itemValue: any, itemIndex: number) => {
+            setIndexCurrency(itemIndex);
+            setSelectedCurrency(itemValue);
+          }}
         >
           {Object.entries(currencyList).map(([key, value]) => (
             <Picker.Item label={key} value={value} key={key} />
           ))}
-          <Picker.Item label="js" value="js" />
         </Picker>
-        <Button title="CONVERT" onPress={() => handleConvert()} />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  homeScreen: {
-    flex: 1,
-  },
-  homeScreen_result: {
+  header: {
     alignItems: "center",
     justifyContent: "flex-start",
     margin: 50,
+    flex: 1,
   },
-  button: {},
+  currencyPicker: {
+    flex: 6,
+  },
+  text: {
+    fontSize: 30,
+  },
+  input: {
+    flexDirection: "row",
+    justifyContent: "center",
+    flex: 1,
+  },
+  textInput: {
+    alignItems: "center",
+    fontSize: 20,
+    width: 50,
+  },
+  textCurrencyUnit: {
+    alignSelf: "center",
+  },
 });
 
 export default HomeScreen;
-{
-  /* {for (const propperty in currencyList) {
-              <Picker.Item label=`${propperty}` value=`${currencyList[propperty]}` />
-          }} */
-}
-{
-  /* <Picker.Item label="JavaScript" value="js" /> */
-}
